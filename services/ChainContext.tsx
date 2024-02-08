e40421293bc72162f96b64b3db32ed10c211fdd5
makushi <maxime.eliazord@sequodia.com>
2024-02-08 01:40:13 +0100

    Fixed mission icon prop

--- services/ChainContext.tsx
+++ services/ChainContext.tsx
@@ -80,6 +80,7 @@ export default function ChainContextProvider(props: ChainContextProviderProps) {
         chainSelector: "0",
         chainId: chain.id.toString(),
         supportedTokens: [],
+        icon: "",
       }
     }
     return (
